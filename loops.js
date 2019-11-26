d0d90aac82d02bb51ec49b997eb84f74a48fb5d0
Miles Higbie <miles.higbie@gmail.com>
2019-11-26 19:20:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,36 +1,31 @@
-var array = [];
-
 function forLoop(array){
-  for (var i = 0; i <25; i++){
-    if (i === 1)  { 
-    array.push(`I am ${i} strange loop.`)
-  } else { 
-    array.push(`I am ${i} strange loops.`)
-  }
+  for(let i = 0; i < 25; i++){
+    if( i === 1){
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
   return array
 }
 
-function whileLoop(n){
-  while(n > 0){
-     console.log(n--)}
-     return "done"
+function whileLoop(num){
+  let i = 0; 
+  while(i < num){
+    console.log(num--)
+  }
+  return(`done`)
 }
 
-
-
-
-function doWhileLoop(num){
-  let i = 0;
-  function incrementVariable() {
+function incrementVariable() {
+  let i = 0; 
   i = i + 1;
   return i;
 }
-do {
+
+function doWhileLoop(int){
+  do {
   console.log("I run once regardless.")
+} while(incrementVariable() < int)
 }
-while (incrementVariable() < num); 
-}
-
-console.log(doWhileLoop())
-
