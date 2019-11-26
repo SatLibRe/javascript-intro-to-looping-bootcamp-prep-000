45aae3e6279038b492c746d2bd7a4a388fb07d8a
Miles Higbie <miles.higbie@gmail.com>
2019-11-26 19:23:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,11 @@
 function forLoop(array){
   for(let i = 0; i < array.length; i++){
-    
+    if(i === 1){
+      array.push(`I am ${i} strange loop`)
+    }
+    else { 
+      array.push(`I am ${i} strange loops`)
   }
+}
+  
 }
